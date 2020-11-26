5a9bdcdf32d9886803f3caacda975dc617b59d8e
CahierJB <jb.cahier@gmail.com>
2020-11-26 23:33:40 +0100

    Connexion DB OK

--- src/main/java/heavenpark/controller/db_access/Provider.java
+++ src/main/java/heavenpark/controller/db_access/Provider.java
@@ -2,7 +2,7 @@ package heavenpark.controller.db_access;
 
 public interface Provider {
     String DRIVER="com.mysql.cj.jdbc.Driver";
-    String CONNECTION_URL="jdbc:mysql://localhost:3306:j2ee_instance";
+    String CONNECTION_URL="jdbc:mysql://localhost:3306";
     String USERNAME="user_java";
     String PASSWORD="U$erJava";
 
